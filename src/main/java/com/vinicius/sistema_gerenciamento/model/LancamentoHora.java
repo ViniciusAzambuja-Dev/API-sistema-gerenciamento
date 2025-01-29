1d5cb481ee96945575175bee3cf68aeda576e39c
Vinicius Azambuja <vinizambs@gmail.com>
2025-01-29 10:50:53 -0300

    feat - Adiciona classe LancamentoHora

---
+++ src/main/java/com/vinicius/sistema_gerenciamento/model/LancamentoHora.java
@@ -0,0 +1,68 @@
+package com.vinicius.sistema_gerenciamento.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "lancamentos_horas")
+public class LancamentoHora {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(max = 100)
+    @NotNull
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String descricao;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate data_inicio;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate data_fim;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime data_registro;
+
+    @ManyToOne
+    @JoinColumn(name = "id_atividade", nullable = false)
+    private Atividade atividade;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario_responsavel;
+
+    public LancamentoHora() {
+    }
+
+    public LancamentoHora(String descricao, LocalDate data_inicio, LocalDate data_fim, Atividade atividade, Usuario usuario_responsavel) {
+        this.descricao = descricao;
+        this.data_inicio = data_inicio;
+        this.data_fim = data_fim;
+        this.atividade = atividade;
+        this.usuario_responsavel = usuario_responsavel;
+    }
+}
