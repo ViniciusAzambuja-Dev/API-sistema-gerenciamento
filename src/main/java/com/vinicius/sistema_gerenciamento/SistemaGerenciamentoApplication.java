ccfe8b4381a9acda38a4d44c94d064fadd828743
Vinicius Azambuja <vinizambs@gmail.com>
2025-01-29 10:45:52 -0300

    feat - Adiciona configurações iniciais de Banco de Dados e dependências do Spring

---
+++ src/main/java/com/vinicius/sistema_gerenciamento/SistemaGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.sistema_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoApplication.class, args);
+	}
+
+}
