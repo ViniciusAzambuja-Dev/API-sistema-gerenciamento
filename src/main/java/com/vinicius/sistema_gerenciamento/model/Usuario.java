8d99632a8395faa219e9f001e505bb5fce8c8336
Vinicius Azambuja <vinizambs@gmail.com>
2025-01-29 10:47:29 -0300

    feat - Adiciona classe Usuario

---
+++ src/main/java/com/vinicius/sistema_gerenciamento/model/Usuario.java
@@ -0,0 +1,65 @@
+package com.vinicius.sistema_gerenciamento.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(max = 50)
+    @NotNull
+    @NotBlank
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    @NotNull
+    @NotBlank
+    @Column(length = 255, nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String senha;
+
+    @NotNull
+    @NotBlank
+    @Column(length = 10, nullable = false)
+    private String perfil;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime data_criacao;
+
+    private LocalDateTime ultimo_login;
+
+    public Usuario(){
+        this.ultimo_login = null;
+    }
+
+    public Usuario(String nome, String email, String senha, String perfil) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+}
