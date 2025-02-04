5c6704bf1670738467fd1142dff34a2eab872774
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-04 18:05:06 -0300

    feat - Adiciona validação da exceção MethodArgumentNotValidException

--- src/main/java/com/vinicius/sistema_gerenciamento/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vinicius/sistema_gerenciamento/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.vinicius.sistema_gerenciamento.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,5 +22,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException exception){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception)  {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + " " + error.getDefaultMessage())
+        .reduce("", (acc, error) -> acc + error + "\n"));
     }
 }
