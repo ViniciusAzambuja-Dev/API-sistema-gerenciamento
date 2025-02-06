552bd79ef034f92ce75c9ab99c0b385ef9ec45e0
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-06 09:38:55 -0300

    feat - Adiciona validação para tirar espaços nos campos da DTO de Login

--- src/main/java/com/vinicius/sistema_gerenciamento/infra/seguranca/dto/LoginRequestDTO.java
+++ src/main/java/com/vinicius/sistema_gerenciamento/infra/seguranca/dto/LoginRequestDTO.java
@@ -7,4 +7,9 @@ import jakarta.validation.constraints.Size;
 public record LoginRequestDTO(
     @NotNull @NotBlank String email,
     @Size(max = 15) @NotNull @NotBlank String senha) {
+        
+        public LoginRequestDTO {
+            email = (email == null || email.isBlank()) ? email : email.trim();
+            senha = (senha == null || senha.isBlank()) ? senha : senha.trim();
+        }
 }
