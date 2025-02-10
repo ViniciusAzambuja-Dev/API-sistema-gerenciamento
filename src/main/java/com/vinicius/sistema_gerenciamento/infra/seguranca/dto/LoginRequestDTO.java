065380e8c871764a2b639b01417012c370142521
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-10 08:46:28 -0300

    fix - Tirando validação desnecessária da DTO de login

--- src/main/java/com/vinicius/sistema_gerenciamento/infra/seguranca/dto/LoginRequestDTO.java
+++ src/main/java/com/vinicius/sistema_gerenciamento/infra/seguranca/dto/LoginRequestDTO.java
@@ -2,11 +2,10 @@ package com.vinicius.sistema_gerenciamento.infra.seguranca.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record LoginRequestDTO(
     @NotNull @NotBlank String email,
-    @Size(max = 15) @NotNull @NotBlank String senha) {
+    @NotNull @NotBlank String senha) {
         
         public LoginRequestDTO {
             email = (email == null || email.isBlank()) ? email : email.trim();
